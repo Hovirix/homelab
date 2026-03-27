92462937a811f3947cf9932742eef42fed765753
Hovirix <184756415+Hovirix@users.noreply.github.com>
2026-03-27 05:42:57 +0100

    refactor: rename secrets files to *.sops.yaml and update references

--- workloads/identity/authentik/terraform/secrets.tf
+++ workloads/identity/authentik/terraform/secrets.tf
@@ -1,7 +1,7 @@
 data "sops_file" "authentik_token" {
-  source_file = "../../secrets/infrastructure/authentik.yaml"
+  source_file = "../../secrets/infrastructure/authentik.sops.yaml"
 }
 
 data "sops_file" "authentik_clients" {
-  source_file = "../../secrets/identity/oauth-clients.yaml"
+  source_file = "../../secrets/identity/oauth-clients.sops.yaml"
 }
