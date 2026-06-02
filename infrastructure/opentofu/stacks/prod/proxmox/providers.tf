46838f36910ef5a52c9c59003dd5bfaf0a22059e
Hovirix <184756415+Hovirix@users.noreply.github.com>
2026-06-02 21:06:55 +0200

    fix(infrastructure): construct proxmox api token from secret value

--- infrastructure/opentofu/stacks/prod/proxmox/providers.tf
+++ infrastructure/opentofu/stacks/prod/proxmox/providers.tf
@@ -2,6 +2,6 @@ provider "sops" {}
 
 provider "proxmox" {
   endpoint  = "https://pve1.home.hovirix.dev:8006/"
-  api_token = data.sops_file.infrastructure.data["proxmox.api_token"]
+  api_token = "opentofu@pve!opentofu=${data.sops_file.infrastructure.data["proxmox.api_token"]}"
   insecure  = true
 }
