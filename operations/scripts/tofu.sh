a0065706a4e33aa38089be21dbacd7c9b859abd1
Hovirix <184756415+Hovirix@users.noreply.github.com>
2026-07-07 12:42:03 +0200

    chore(operations): simplify opentofu task ux

--- operations/scripts/tofu.sh
+++ operations/scripts/tofu.sh
@@ -3,16 +3,15 @@
 set -euo pipefail
 
 main() {
-  local cmd="${1:?missing tofu command}"
-  local sops_file="${2:?missing sops file}"
+  local sops_file="${1:?missing sops file}"
 
-  shift 2
+  shift 1
 
   TF_VAR_opentofu_state_encryption_passphrase="$(
     sops -d \
       --extract '["opentofu"]["state_encryption_passphrase"]' \
       "$sops_file"
-  )" tofu "$cmd" "$@"
+  )" tofu "$@"
 }
 
 main "$@"
