5ec9fade307b5153ba72456fc535900bd6bd482c
hovirix <contact@hovirix.dev>
2026-07-12 13:29:52 +0100

    fix(operations): make sops key apply idempotent

--- operations/scripts/apply-sops-key.sh
+++ operations/scripts/apply-sops-key.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+kubectl delete secret sops-age -n flux-system --ignore-not-found
+
 sops -d ./secrets/platform.sops.yaml | yq ".flux.sops_age_key" |
   kubectl create secret generic sops-age \
     --namespace=flux-system \
