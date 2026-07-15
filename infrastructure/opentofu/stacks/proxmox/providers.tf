3b6acc1180210a4ed792841eff14570cd6d10600
hovirix <contact@hovirix.dev>
2026-07-15 19:44:00 +0100

    fix(infrastructure): verify proxmox provider tls

--- infrastructure/opentofu/stacks/proxmox/providers.tf
+++ infrastructure/opentofu/stacks/proxmox/providers.tf
@@ -3,5 +3,4 @@ provider "sops" {}
 provider "proxmox" {
   endpoint  = "https://pve1.home.hovirix.dev:8006/"
   api_token = "opentofu@pve!opentofu=${data.sops_file.infrastructure.data["proxmox.api_token"]}"
-  insecure  = true
 }
