6a7bfc5bd17a88423b890dabdc5b10c9cbafe21d
hovirix <contact@hovirix.dev>
2026-07-15 18:37:21 +0100

    feat(infrastructure): migrate opentofu state to cloudflare r2

--- infrastructure/opentofu/stacks/talos/backend.tf
+++ infrastructure/opentofu/stacks/talos/backend.tf
@@ -4,6 +4,23 @@ variable "opentofu_state_encryption_passphrase" {
 }
 
 terraform {
+  backend "s3" {
+    bucket = "tf-state"
+    key    = "prod/talos/terraform.tfstate"
+    region = "auto"
+
+    endpoints = {
+      s3 = "https://2bcc2c0c19d1ad6037c19ed8fe4a0043.r2.cloudflarestorage.com"
+    }
+
+    use_path_style              = true
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    skip_s3_checksum            = true
+  }
+
   encryption {
     key_provider "pbkdf2" "state" {
       passphrase    = var.opentofu_state_encryption_passphrase
--- operations/scripts/tofu.sh
+++ operations/scripts/tofu.sh
@@ -3,14 +3,24 @@
 set -euo pipefail
 
 main() {
-  local sops_file="${1:?missing sops file}"
+  local infrastructure_sops_file="${1:?missing infrastructure sops file}"
 
   shift 1
 
+  AWS_ACCESS_KEY_ID="$(
+    sops -d \
+      --extract '["opentofu"]["state"]["backend"]["access_key_id"]' \
+      "$infrastructure_sops_file"
+  )" \
+  AWS_SECRET_ACCESS_KEY="$(
+    sops -d \
+      --extract '["opentofu"]["state"]["backend"]["secret_access_key"]' \
+      "$infrastructure_sops_file"
+  )" \
   TF_VAR_opentofu_state_encryption_passphrase="$(
     sops -d \
-      --extract '["opentofu"]["state_encryption_passphrase"]' \
-      "$sops_file"
+      --extract '["opentofu"]["state"]["encryption_passphrase"]' \
+      "$infrastructure_sops_file"
   )" tofu "$@"
 }
 
