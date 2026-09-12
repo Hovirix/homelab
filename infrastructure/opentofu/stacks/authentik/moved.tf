1c776c0f9ddac96b76df3f7169b4248258ee53a6
hovirix <contact@hovirix.dev>
2026-09-12 14:36:19 +0200

    refactor(infrastructure): remove immich authentik resources

--- infrastructure/opentofu/stacks/authentik/moved.tf
+++ infrastructure/opentofu/stacks/authentik/moved.tf
@@ -4,11 +4,6 @@ moved {
 }
 
 moved {
-  from = authentik_provider_oauth2.app["immich"]
-  to   = authentik_provider_oauth2.immich
-}
-
-moved {
   from = authentik_provider_oauth2.app["paperless"]
   to   = authentik_provider_oauth2.paperless
 }
